edf93fccd09ffb34467a3e37184ee02d8c6e71e4
devsgnr <hi@devsgnr.xyz>
2022-06-24 22:32:09 +0100

    Removed GSAP - will uninstall later

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,11 @@
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 import { NextPage } from 'next';
 import ReactHtmlParser from 'react-html-parser';
-import { gsap, Power3 } from 'gsap';
 import { FetchHomesService } from './api/home';
 import Layout from '../components/layout';
-import { Paragraph } from '../components/typography/styled';
+import { Heading, Paragraph } from '../components/typography/styled';
 import { IHomeProps } from '../types/home';
 import { IHomeResponse } from '../types/response';
-import AnimatedHeading from '../components/animated-heading';
 
 export const getStaticProps = async () => {
   const res: IHomeResponse<IHomeProps[]> = await FetchHomesService();
@@ -28,41 +26,18 @@ interface HomePageProps {
 }
 
 const Home: NextPage<HomePageProps> = ({ data }: HomePageProps) => {
-  const ContainerRef = useRef(null);
-  const P1Ref = useRef(null);
-  const P2Ref = useRef(null);
-
-  useEffect(() => {
-    gsap
-      .timeline()
-      .to(ContainerRef.current, {
-        duration: 0,
-        opacity: 1,
-        ease: Power3.easeIn,
-      })
-      .to([P1Ref.current, P2Ref.current], {
-        duration: 0.6,
-        opacity: 1,
-        ease: Power3.easeIn,
-      });
-  }, []);
-
   return (
     <Layout>
-      <div ref={ContainerRef} style={{ opacity: 0 }}>
-        <AnimatedHeading>
+      <div>
+        <Heading className="title">
           Hello, I&apos;m Emmanuel. A full-stack creator, obsessed with crafting
           beautiful interfaces and experiences through the combination of
           writing, design, code, and no-code
-        </AnimatedHeading>
+        </Heading>
 
-        <Paragraph ref={P1Ref} css={{ opacity: 0 }}>
-          {ReactHtmlParser(data.about.html)}
-        </Paragraph>
+        <Paragraph>{ReactHtmlParser(data.about.html)}</Paragraph>
 
-        <Paragraph ref={P2Ref} css={{ opacity: 0 }}>
-          {ReactHtmlParser(data.timeline.html)}
-        </Paragraph>
+        <Paragraph>{ReactHtmlParser(data.timeline.html)}</Paragraph>
       </div>
     </Layout>
   );
--- pages/writing/index.tsx
+++ pages/writing/index.tsx
@@ -1,7 +1,6 @@
 import Link from 'next/link';
 import { NextPage } from 'next';
-import React, { useEffect, useRef } from 'react';
-import { gsap, Power3 } from 'gsap';
+import React from 'react';
 import { FetchPostsService } from '../api/posts';
 import { StyledInternalA } from '../../components/anchor/styled';
 import BlogItem from '../../components/blog-item/styled';
@@ -21,47 +20,16 @@ export const getStaticProps = async () => {
     props: {
       data: res.posts,
     },
+    revalidate: 3600,
   };
 };
 
 const Writing: NextPage<BlogsPageProps> = ({ data }: BlogsPageProps) => {
-  const ContainerRef = useRef(null);
-  const BlogItemRef = useRef(null);
-
-  useEffect(() => {
-    gsap.to(ContainerRef.current, {
-      duration: 0,
-      opacity: 1,
-      ease: Power3.easeIn,
-    });
-
-    data.forEach((value: IBlogProps, index: number) => {
-      gsap
-        .timeline({ delay: index * 0.3 })
-        .from(`.${value.slug}`, {
-          duration: 0.2,
-          opacity: 0,
-          ease: Power3.easeOut,
-        })
-        .to(`.${value.slug}`, {
-          duration: 0.2,
-          opacity: 1,
-          ease: Power3.easeOut,
-          stagger: 0.25,
-        });
-    });
-  }, []);
-
   return (
     <Layout>
-      <div ref={ContainerRef} style={{ opacity: 0 }}>
+      <div>
         {data.map((item: IBlogProps, index: number) => (
-          <div
-            className={item.slug}
-            key={index}
-            ref={BlogItemRef}
-            style={{ opacity: 0 }}
-          >
+          <div key={index}>
             <Link
               passHref
               href={{ pathname: '/writing/[slug]', query: { slug: item.slug } }}
