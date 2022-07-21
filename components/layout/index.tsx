139c5946ebc5655440279b6e07718a73f8bded03
devsgnr <hi@devsgnr.xyz>
2022-07-21 16:59:48 +0100

    Coming Soon

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,10 +1,8 @@
 import React, { ReactChild, useEffect, useRef } from 'react';
 import { gsap, Power3 } from 'gsap';
 import { ISeo } from '../../types/seo';
-import NavigationBar from '../navbar';
 import SEO from '../seo';
 import StyledLayout from './styled';
-import Footer from '../footer-links';
 
 interface LayoutProps {
   children: ReactChild | ReactChild[];
